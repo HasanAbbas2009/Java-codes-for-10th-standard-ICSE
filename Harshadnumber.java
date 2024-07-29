0c506545d62a15ef069995c7250c461914f4be1a
Hasan <hasanabbaskazim@gmail.com>
2024-07-29 14:51:29 +0530

    Update Harshadnumber.java

--- Harshadnumber.java
+++ Harshadnumber.java
@@ -1,3 +1,4 @@
+//Harshad number A number is said to be the Harshad number if it is divisible by the sum of its digit. For example, if number is 156, then sum of its digit will be 1 + 5 + 6 = 12. Since 156 is divisible by 12. So, 156 is a Harshad number
 public class Harshadnumber
 {
         public static void main(String[] args) {
